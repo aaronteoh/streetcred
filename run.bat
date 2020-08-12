2db69dda8768ad97a294a628d3b536c26dcf1996
Aaron <teohkangkaiaaron@gmail.com>
2020-08-12 18:12:53 +0800

    create bat file to run on task scheduler

---
+++ run.bat
@@ -0,0 +1,3 @@
+C:\Users\aaron\Anaconda3\python.exe "C:/Users/aaron/Documents/streetcred/download_images.py"
+C:\Users\aaron\Anaconda3\python.exe "C:/Users/aaron/Documents/streetcred/detect_objects.py"
+pause
