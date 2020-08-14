b16a977832fc03ccbac8e0431ea15e6ca89bcaf7
Aaron <teohkangkaiaaron@gmail.com>
2020-08-14 21:49:23 +0800

    log object class index instead of class aggregate detections after generating detections added functions to load detections and aggregrates to GCS, then remove from local other edits to tidy up outputs

--- detect_objects.py
+++ detect_objects.py
@@ -4,43 +4,6 @@ from datetime import datetime
 from logger import load_logger, logging
 
 
-def check_dir_name(dir_name):
-    try:
-        datetime.strptime(dir_name, '%Y%m%d%H%M')
-    except ValueError:
-        return False
-    else:
-        return True
-
-
-def load_image_into_numpy_array(path):
-    img_data = tf.io.gfile.GFile(path, 'rb').read()
-    image = Image.open(BytesIO(img_data))
-    (im_width, im_height) = image.size
-
-    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
-
-
-def get_keypoint_tuples(eval_config):
-    tuple_list = []
-    kp_list = eval_config.keypoint_edge
-    for edge in kp_list:
-        tuple_list.append((edge.start, edge.end))
-
-    return tuple_list
-
-
-def load_label_map():
-    label_map_path = os.path.join(proj_dir, 'object_detection/data/mscoco_label_map.pbtxt')
-    label_map = label_map_util.load_labelmap(label_map_path)
-    categories = label_map_util.convert_label_map_to_categories(label_map,
-                                                                max_num_classes=label_map_util.get_max_label_map_index(label_map),
-                                                                use_display_name=True)
-    category_index = label_map_util.create_category_index(categories)
-
-    return category_index
-
-
 def load_model():
     model_name = 'centernet_resnet101_v1_fpn_512x512_coco17_tpu-8'
     pipeline_config = os.path.join(proj_dir, 'object_detection', 'test_data', model_name, 'pipeline.config')
@@ -69,6 +32,31 @@ def get_model_detection_function(model):
     return detect_fn
 
 
+def main():
+    completed_dirs = []
+    for root, dirs, files in os.walk(metadata_dir):
+        for file in files:
+            completed_dirs.append(os.path.join(file)[:12])
+
+    to_process = [dir_name for dir_name in os.listdir(images_dir) if check_dir_name(dir_name) and dir_name in completed_dirs]
+    logging.info('%s directories to process'%len(to_process))
+
+    for dir_name in to_process:
+        detections_df = run_detections_for_dir(dir_name)
+        aggregated_df = aggregate_detections(detections_df)
+        compile_detections(aggregated_df, dir_name)
+        clear_working_files(dir_name)
+
+
+def check_dir_name(dir_name):
+    try:
+        datetime.strptime(dir_name, '%Y%m%d%H%M')
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 def run_detections_for_dir(dir_name):
     logging.info('Running detections for %s'%dir_name)
     images = [x for x in os.listdir(os.path.join(images_dir, dir_name)) if x.endswith('.jpg')]
@@ -76,8 +64,6 @@ def run_detections_for_dir(dir_name):
 
     logging.info('%s images to process' % len(images))
     for image_file in images:
-        # logging.info('Processing %s.' % image_file)
-
         # possible to stack?
         image_path = os.path.join(images_dir, dir_name, image_file)
         image_np = load_image_into_numpy_array(image_path)
@@ -89,35 +75,93 @@ def run_detections_for_dir(dir_name):
         for i in range(detections['num_detections'].numpy()[0]):
             score = detections['detection_scores'][:, i].numpy()[0]
             if score > .2:
-                object_class = category_index[detections['detection_classes'][:, i].numpy()[0] + label_id_offset]['name']
+                object_class = detections['detection_classes'][:, i].numpy()[0] + label_id_offset
                 box = detections['detection_boxes'][:, i, :].numpy()[0]
                 relative_size = (box[2] - box[0]) * (box[3] - box[1])
 
-                compiled.append([image_file,
+                compiled.append([image_file.split('.')[0],
                                  object_class,
                                  score,
                                  box[1], box[3], box[0], box[2],
                                  relative_size])
 
+    if not os.path.isdir(detections_dir):
+        os.makedirs(detections_dir)
+        logging.info('Created %s' % detections_dir)
+
     detections_path = os.path.join(detections_dir, '%s_detections.csv' % dir_name)
-    pd.DataFrame(compiled, columns=['image', 'class', 'score', 'x1', 'x2', 'y1', 'y2', 'relative_size']).to_csv(detections_path, index=False)
+    detections_df = pd.DataFrame(compiled, columns=['image', 'class', 'score', 'x1', 'x2', 'y1', 'y2', 'relative_size'])
+    logging.info('Detections DF shape: %s' % str(detections_df.shape))
+    detections_df.to_csv(detections_path, index=False, header=False)
     logging.info('Saved detections to %s' % detections_path)
 
+    upload_blob(detections_path, 'detections/%s_detections.csv' % dir_name)
+    os.remove(detections_path)
+    logging.info('Deleted %s' % detections_path)
 
-def main():
-    to_process = [dir_name for dir_name in os.listdir(images_dir) if check_dir_name(dir_name)]
-    logging.info('%s directories to process'%len(to_process))
+    return detections_df
 
-    for dir_name in to_process:
-        run_detections_for_dir(dir_name)
 
-        from_dir = os.path.join(images_dir, dir_name)
-        # to_dir = os.path.join(images_dir, 'p' + dir_name)
-        # os.rename(from_dir, to_dir)
-        # logging.info('Images moved from %s to %s'%(from_dir, to_dir))
+def load_image_into_numpy_array(path):
+    img_data = tf.io.gfile.GFile(path, 'rb').read()
+    image = Image.open(BytesIO(img_data))
+    (im_width, im_height) = image.size
 
-        logging.info('Deleting %s' % from_dir)
-        shutil.rmtree(from_dir)
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+
+
+def upload_blob(source_file_name, destination_blob_name):
+    bucket = storage_client.bucket('tyeoh-streetcred')
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    logging.info("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
+
+
+def aggregate_detections(detections_df):
+    detections_df['rank'] = detections_df.groupby(by=['image', 'x1', 'x2', 'y1', 'y2'])['score'].transform(lambda x: x.rank(method='first', ascending=False))
+
+    detections_df = detections_df[(detections_df['score'] > .3) &
+                                  (detections_df['relative_size'] < .1) &
+                                  (detections_df['rank'] == 1) &
+                                  (detections_df['class'].isin([3,4,6,8]))][['image']]
+    aggregated_df = detections_df.groupby('image').agg(Detections=('image', 'count')).reset_index()
+    aggregated_df.drop_duplicates(inplace=True)
+    logging.info('Aggregated DF shape: %s' % str(aggregated_df.shape))
+
+    return aggregated_df
+
+
+def compile_detections(aggregated_df, dir_name):
+    metadata_path = os.path.join(metadata_dir, '%s_images.csv' % dir_name)
+    metadata_df = pd.read_csv(metadata_path, names = ['CameraID', 'Latitude', 'Longitude', 'Date', 'Time', 'Filename', 'Dimensions']).drop_duplicates()
+
+    combined = metadata_df.merge(aggregated_df, left_on='Filename', right_on='image', how='left').drop(columns=['image'])
+    combined['Detections'].fillna(0, inplace=True)
+    logging.info('Combined DF shape: %s' % str(combined.shape))
+
+    if not os.path.isdir(aggregated_dir):
+        os.makedirs(aggregated_dir)
+        logging.info('Created %s' % aggregated_dir)
+
+    aggregated_path = os.path.join(aggregated_dir, '%s_aggregated.csv' % dir_name)
+    combined.to_csv(aggregated_path, index=False, header=False)
+    logging.info('Saved aggregated data to %s' % aggregated_path)
+
+    upload_blob(aggregated_path, 'aggregated/%s_aggregated.csv' % dir_name)
+    os.remove(aggregated_path)
+    logging.info('Deleted %s' % aggregated_path)
+
+
+def clear_working_files(dir_name):
+    images_path = os.path.join(images_dir, dir_name)
+    shutil.rmtree(images_path)
+    logging.info('Deleted %s' % images_path)
+
+    metadata_path = os.path.join(metadata_dir, '%s_images.csv' % dir_name)
+    os.remove(metadata_path)
+    logging.info('Deleted %s' % metadata_path)
+
 
 if __name__ == '__main__':
     proj_dir = pathlib.Path(__file__).parent.absolute()
@@ -130,6 +174,8 @@ if __name__ == '__main__':
         data_dir = os.path.join(proj_dir, 'data')
         images_dir = os.path.join(data_dir, 'images')
         detections_dir = os.path.join(data_dir, 'detections')
+        metadata_dir = os.path.join(data_dir, 'metadata')
+        aggregated_dir = os.path.join(data_dir, 'aggregated')
 
         import shutil
         import numpy as np
@@ -142,20 +188,16 @@ if __name__ == '__main__':
         logging.info('Base libraries loaded')
 
         from object_detection.utils import config_util
-        from object_detection.utils import label_map_util
         from object_detection.builders import model_builder
         logging.info('Object detection modules loaded')
 
-        if not os.path.isdir(detections_dir):
-            logging.info('Creating %s' % detections_dir)
-            os.makedirs(detections_dir)
+        from google.cloud import storage
+        key_path = os.path.join(pathlib.Path(__file__).parent.absolute(), 'credentials','tyeoh-cloud-da52d6b77ebd.json')
+        storage_client = storage.Client.from_service_account_json(key_path)
 
         detection_model, configs = load_model()
         detect_fn = get_model_detection_function(detection_model)
         logging.info('Model loaded.')
-
-        category_index = load_label_map()
-        logging.info('Label map loaded.')
 
         main()
     except Exception as e:
