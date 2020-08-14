bf1c3024b75f03a1dca46d7d6b4cb6cb95aad529
Aaron <teohkangkaiaaron@gmail.com>
2020-08-14 17:55:35 +0800

    split running of download and detect into separate bat files

---
+++ run_detect.bat
@@ -0,0 +1,1 @@
+C:\Users\aaron\Anaconda3\python.exe "C:/Users/aaron/Documents/streetcred/detect_objects.py"
---
+++ run_download.bat
@@ -0,0 +1,1 @@
+C:\Users\aaron\Anaconda3\python.exe "C:/Users/aaron/Documents/streetcred/download_images.py"
